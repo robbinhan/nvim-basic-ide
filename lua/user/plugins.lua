26aabaefe43ed4cc0a7f0f4a9e2c89b86c0a3f4e
robbin <luckyhanbin@gmail.com>
2024-03-31 20:26:24 +0800

    add plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -14,6 +14,40 @@ vim.opt.rtp:prepend(lazypath)
 -- Install your plugins here
 return require("lazy").setup({
   {
+    "Mr-LLLLL/lualine-ext.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lualine/lualine.nvim",
+      -- if you want to open telescope window when click on LSP info of lualine, uncomment it
+      -- "nvim-telescope/telescope.nvim"
+    },
+    opts = {
+      separator = {
+        left = "",
+        right = "",
+      },
+      init_tab_project = {
+        disabled = false,
+        -- set this for your colorscheme. I have not default setting in diff colorcheme.
+        tabs_color = {
+          inactive = {
+            fg = "#9da9a0",
+            bg = "#4f5b58",
+          },
+        }
+      },
+      init_lsp = {
+        disabled = false,
+      },
+      init_tab_date = true,
+    }
+  },
+  {
+    "zeioth/garbage-day.nvim",
+    dependencies = "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+  },
+  {
     "rolv-apneseth/tfm.nvim",
     lazy = false,
     config = function()
